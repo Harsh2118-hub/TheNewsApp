b6930535c2390d1b5e10f51fe3548048e5ae8213
harsh jain <22bit125@ietdavv.edu.in>
2025-02-22 22:36:51 +0530

    added room database

--- app/src/main/java/com/example/thenewsapp/db/ArticleDatabase.kt
+++ app/src/main/java/com/example/thenewsapp/db/ArticleDatabase.kt
@@ -4,10 +4,9 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
 import androidx.room.TypeConverters
 import com.example.thenewsapp.models.Article
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.internal.synchronized
 
 @Database(
     entities = [Article::class],
@@ -23,8 +22,7 @@ abstract class ArticleDatabase : RoomDatabase() {
         private var instance: ArticleDatabase? = null
         private val LOCK = Any()
 
-        @OptIn(InternalCoroutinesApi::class)
-        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK){
             instance ?: createDatabase(context).also {
                 instance = it
 
