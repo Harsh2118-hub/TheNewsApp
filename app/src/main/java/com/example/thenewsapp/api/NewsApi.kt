a870ffea6ba724c897a6c0cd542e1cba86409986
harsh jain <22bit125@ietdavv.edu.in>
2025-02-22 19:13:35 +0530

    Added RetrofitInstance for API calls

--- app/src/main/java/com/example/thenewsapp/api/NewsApi.kt
+++ app/src/main/java/com/example/thenewsapp/api/NewsApi.kt
@@ -1,6 +1,7 @@
 package com.example.thenewsapp.api
 
 import com.example.thenewsapp.models.NewsResponse
+import com.example.thenewsapp.util.Constants.Companion.API_KEY
 import okhttp3.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -17,10 +18,16 @@ interface NewsApi {
         pageNumber: Int = 1,
         @Query("apiKey")
         apiKey: String = API_KEY
-    ): Response<NewsResponse>
+    ): retrofit2.Response<NewsResponse>
 
     @GET("v2/everything")
     suspend fun searchForNews(
-
-    )
+        @Query("country")
+        countryCode: String = "us",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): retrofit2.Response<NewsResponse>
+    
 }
